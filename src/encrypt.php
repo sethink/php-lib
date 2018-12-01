a618bbe8c85c6d8cbb4134742bf4aa91b4cc4554
皮皮虾皮 <358066237@qq.com>
2018-12-01 11:41:44 +0800

    modify

--- src/encrypt.php
+++ src/encrypt.php
@@ -1,61 +1,21 @@
 <?php
 
-/**
- * @加密
- *
- * @param $txt @加密字符
- * @param string $key @key
- * @return string
- */
-function passport_encrypt($txt, $key = '5cb1bec5931b5476b3162a697fb50ddf')
-{
-    srand((double)microtime() * 1000000);
-    $encrypt_key = md5(rand(0, 32000));
-    $ctr         = 0;
-    $tmp         = '';
-    for ($i = 0; $i < strlen($txt); $i++) {
-        $ctr = $ctr == strlen($encrypt_key) ? 0 : $ctr;
-        $tmp .= $encrypt_key[$ctr] . ($txt[$i] ^ $encrypt_key[$ctr++]);
-    }
-    return base64_encode(passport_key($tmp, $key));
-}
+namespace sethink\functionLib;
 
+use sethink\functionLib\map\EncryptMap;
 
 /**
- * @解密
- *
- * @param $txt @解密字符
- * @param string $key @key
- * @return string
+ * Class Encrypt
+ * @package sethink\functionLib\map\EncryptMap
+ * @method EncryptMap passport_encrypt(string $txt, string $key = '') static
+ * @method EncryptMap passport_decrypt(string $txt, string $key = '') static
  */
-function passport_decrypt($txt, $key = '5cb1bec5931b5476b3162a697fb50ddf')
+class Encrypt
 {
-    $txt = passport_key(base64_decode($txt), $key);
-    $tmp = '';
-    for ($i = 0; $i < strlen($txt); $i++) {
-        $md5 = $txt[$i];
-        $tmp .= $txt[++$i] ^ $md5;
+    public static function __callStatic($method, $args)
+    {
+
+        $class = '\\sethink\\functionLib\\map\\EncryptMap';
+        return call_user_func_array([new $class, $method], $args);
     }
-
-    return $tmp;
-}
-
-
-/**
- * @生成密钥
- *
- * @param $txt
- * @param $encrypt_key
- * @return string
- */
-function passport_key($txt, $encrypt_key)
-{
-    $encrypt_key = md5($encrypt_key);
-    $ctr         = 0;
-    $tmp         = '';
-    for ($i = 0; $i < strlen($txt); $i++) {
-        $ctr = $ctr == strlen($encrypt_key) ? 0 : $ctr;
-        $tmp .= $txt[$i] ^ $encrypt_key[$ctr++];
-    }
-    return $tmp;
 }
