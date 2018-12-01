5fdac73f15384fe8ab45e18aefde377e3886e852
皮皮虾皮 <358066237@qq.com>
2018-12-01 11:38:12 +0800

    modify

---
+++ src/SplString.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace sethink\functionLib;
+
+use sethink\functionLib\map\SplStringMap;
+
+/**
+ * Class SplString
+ * @package sethink\functionLib\map\SplStringMap
+ * @method SplStringMap str2ascii(string $str) static
+ * @method SplStringMap ascii2str(string $ascii) static
+ */
+class SplString
+{
+
+    public static function __callStatic($method, $args)
+    {
+        $class = '\\sethink\\functionLib\\map\\SplStringMap';
+        return call_user_func_array([new $class, $method], $args);
+    }
+
+}
