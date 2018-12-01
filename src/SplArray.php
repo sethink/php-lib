67edff44d343df3cf3e43290a4b77251fedea5d9
皮皮虾皮 <358066237@qq.com>
2018-12-01 11:49:25 +0800

    modify

--- src/SplArray.php
+++ src/SplArray.php
@@ -7,8 +7,8 @@ use sethink\functionLib\map\SplArrayMap;
 /**
  * Class SplArray
  * @package sethink\functionLib\map\SplArrayMap
- * @method SplArrayMap deleteValue(array $delArray, array $array) static    删除指定值
- * @method SplArrayMap getColumnValue(string $column, array $array) static  获取指定列的值(多维数组)
+ * @method SplArrayMap deleteValue(array $delArray, array $array) static 删除指定值
+ * @method SplArrayMap getColumnValue(string $column, array $array) static 获取指定列的值(多维数组)
  * @method SplArrayMap unique(array $array) static
  * @method SplArrayMap multiple(array $array) static
  */
