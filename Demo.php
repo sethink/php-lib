4e36c8539f856f65f4beaf4336544c6fcfd81e51
皮皮虾皮 <358066237@qq.com>
2018-12-13 13:22:02 +0800

    modify

--- Demo.php
+++ Demo.php
@@ -7,21 +7,19 @@ include_once "./src/SplString.php";
 include_once "./src/map/SplStringMap.php";
 include_once "./src/Common.php";
 include_once "./src/map/CommonMap.php";
+include_once "./src/Validate.php";
+include_once "./src/map/ValidateMap.php";
 
 use sethink\functionLib\SplArray;
 use sethink\functionLib\Encrypt;
 use sethink\functionLib\SplString;
 use sethink\functionLib\Common;
+use sethink\functionLib\Validate;
 
 class Demo
 {
     public function index(){
-        $array = [
-            '1','2','3','4','5','2','4','6'
-        ];
-
-        $array = SplArray::multiple($array);
-        var_dump($array);
+        var_dump(Validate::boolean('1'));
     }
 }
 
---
+++ src/Validate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace sethink\functionLib;
+
+use sethink\functionLib\map\ValidateMap;
+
+/**
+ * Class Validate
+ * @package sethink\functionLib\map\ValidateMap
+ * @method ValidateMap required(string $string) static 必填
+ * @method ValidateMap date(string $string) static 有效时间
+ * @method ValidateMap alpha(string $string) static 只允许字母
+ * @method ValidateMap alphaNum(string $string) static 只允许字母和数字
+ * @method ValidateMap alphaDash(string $string) static 只允许字母、数字和下划线 破折号
+ * @method ValidateMap chs(string $string) static 只允许汉字
+ * @method ValidateMap chsAlpha(string $string) static 只允许汉字、字母
+ * @method ValidateMap chsAlphaNum(string $string) static 只允许汉字、字母和数字
+ * @method ValidateMap chsDash(string $string) static 只允许汉字、字母、数字和下划线_及破折号-
+ * @method ValidateMap activeUrl(string $string) static 是否为有效的网址(不带http)
+ * @method ValidateMap ip(string $string) static 是否为IP地址
+ * @method ValidateMap url(string $string) static 是否为一个URL地址
+ * @method ValidateMap float(string $string) static 是否为float
+ * @method ValidateMap number(string $string) static 是否为数字
+ * @method ValidateMap integer(string $string) static 是否为整型
+ * @method ValidateMap email(string $string) static 是否为邮箱地址
+ * @method ValidateMap boolean(string $string) static 是否为布尔值
+ * @method ValidateMap isArray(string $string) static 是否为数组
+ */
+class Validate
+{
+
+    public static function __callStatic($method, $args)
+    {
+        $class = '\\sethink\\functionLib\\map\\ValidateMap';
+        return call_user_func_array([new $class, $method], $args);
+    }
+
+}
